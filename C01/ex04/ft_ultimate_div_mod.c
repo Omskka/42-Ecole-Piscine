2b61347060c1ccd3899ee629a2f8749becea113c
Omskka <92455486+Omskka@users.noreply.github.com>
2022-09-09 19:35:53 +0300

    Add files via upload

---
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okirca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 10:33:16 by okirca            #+#    #+#             */
+/*   Updated: 2022/08/30 10:35:40 by okirca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b)
+{
+	int	div;
+	int	mod;
+
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
+	printf("%d", *a);
+	printf("%d", *b);
+}
+
+int	main(void)
+{
+	int	x;
+	int	y;
+
+	x = 15;
+	y = 3;
+	ft_ultimate_div_mod(&x, &y);
+}
